34f6574b272abd5d56764c9f43f2d978208658df
symax <maximsukhanov@gmail.com>
2014-06-13 20:10:08 +0700

    fix

--- ConsoleApplication5/ConsoleApplication5/Odds.cs
+++ ConsoleApplication5/ConsoleApplication5/Odds.cs
@@ -10,14 +10,14 @@ namespace ConsoleApplication5
     {
         public static void PreFlopOdds(Card h1, Card h2)
         {
-            double odds = 1;
+            double odds = 100;
             if (h1.Value != h2.Value)
             {
-                odds = 6 * 100 / 50;
+                odds *= 6.0 / 50;
             }
             else
-                odds = 2 * 100 / 50;
-            Console.WriteLine("Ваши шансы улучшить руку = " + odds.ToString() + "%");
+                odds *= 2.0 / 50;
+            Console.WriteLine("Ваши шансы улучшить руку = " + odds.ToString(".00") + "%");
         }
 
         public static void FlopOdds(Card h1, Card h2, Card d1, Card d2, Card d3)
